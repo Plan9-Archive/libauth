f8eecce70c30c12aa0718748b826239d05ced649
Nick Owens <mischief@offblast.org>
2016-09-23 14:59:05 -0700

    fix tokenize by mimicing plan9 tokenize(2)

--- common_test.go
+++ common_test.go
@@ -24,3 +24,22 @@ func TestAttrMap(t *testing.T) {
 		}
 	}
 }
+
+func TestTokenize(t *testing.T) {
+	for i, tt := range []struct {
+		in     string
+		fields int
+	}{
+		{"ok a ''", 3},
+		{"ok a b", 3},
+		{"ok '' ''", 3},
+		{"ok '' b", 3},
+	} {
+		spl := tokenize(tt.in)
+		if len(spl) != tt.fields {
+			t.Errorf("%d: expected %d fields, got %d", i, tt.fields, len(spl))
+		}
+
+		t.Logf("%d: fields: %q", i, spl)
+	}
+}
